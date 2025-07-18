ebaec5a2aaaca9299c8046b6d385d76853a8d99a
ankushnaudiyal09 <ankushnaudiyal09@gmail.com>
2025-07-18 15:30:45 +0530

    added task

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 
+let tasks = [
+  { id: 1, title: "Learn Docker", completed: false },
+  { id: 2, title: "Set up CI/CD", completed: false },
+  { id: 3, title: "Deploy to production", completed: false },
+  { id: 1, title: "Learn Docker", completed: false },
+  { id: 2, title: "Set up CI/CD", completed: false },
+  { id: 3, title: "Deploy to production", completed: false },
+  { id: 1, title: "Learn Docker", completed: false },
+  { id: 2, title: "Set up CI/CD", completed: false },
+  { id: 3, title: "Deploy to production", completed: false },
+];
 
-app.get("/", (req, res) => res.send("Hello Docker CI/CD!"));
+
+app.get("/", (req, res) => res.send(tasks));
 
 app.listen(3000, () => console.log("Running on port 3000"));
